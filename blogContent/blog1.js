9e18bea4f3619fa0a8e2751b03b8ac0d28479f35
Azyys <146501283+azyys01@users.noreply.github.com>
2024-05-04 11:39:21 +0700

    Add files via upload

---
+++ blogContent/blog1.js
@@ -0,0 +1,7 @@
+document.getElementById('arrow').addEventListener("click", function() {
+  window.location.href = "../blog.html";
+});
+
+document.getElementById('blog1Profile').addEventListener("click", function() {
+  window.location.href = "https://azyys01.github.io/Testing-Web/";
+});
